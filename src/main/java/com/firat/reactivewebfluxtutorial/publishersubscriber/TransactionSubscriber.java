77781a08d7ce183a05b809024b174949c4da1233
Fırat Payalan <yfpayalan@gmail.com>
2019-10-04 15:31:54 +0300

    improvements

--- src/main/java/com/firat/reactivewebfluxtutorial/publishersubscriber/TransactionSubscriber.java
+++ src/main/java/com/firat/reactivewebfluxtutorial/publishersubscriber/TransactionSubscriber.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 public class TransactionSubscriber implements Subscriber<Transaction> {
     Logger logger = LoggerFactory.getLogger(TransactionSubscriber.class);
 
+    private Transaction transaction;
     private Subscription subscription;
 
     @Override
@@ -18,7 +19,13 @@ public class TransactionSubscriber implements Subscriber<Transaction> {
 
     @Override
     public void onNext(Transaction transaction) {
-        logger.info("Publisher published the item: {}, {}",transaction.toString(),Thread.currentThread().getName());
+        this.transaction = transaction;
+        // In the Mono object we use it within doOnNext() method. For example;
+        // Mono.just(transaction)
+        // .doOnNext(transaction->transaction.setStatus("IN PROGRESS")
+        // .doOnNext(transaction->logger.info("onNext() the item: {}, {}",transaction.toString(),Thread.currentThread().getName()))
+        transaction.setStatus("IN PROGRESS");
+        logger.info("onNext() the item: {}, {}",transaction.toString(),Thread.currentThread().getName());
 //        this.subscription.request(1);
     }
 
@@ -29,6 +36,13 @@ public class TransactionSubscriber implements Subscriber<Transaction> {
 
     @Override
     public void onComplete() {
+        // In the Mono object we use it within doOnSuccess() method. For example;
+        // Mono.just(transaction)
+        // .doOnSuccess(transaction->transaction.setStatus("FINISHED")
+        // .doOnSuccess(transaction->logger.info("onNext() the item: {}, {}",transaction.toString(),Thread.currentThread().getName()))
+
+        this.transaction.setStatus("FINISHED");
+        logger.info("onComplete() the item: {}, {}",transaction.toString(),Thread.currentThread().getName());
         logger.info("Transaction Processed");
     }
 }
--- src/test/java/com/firat/reactivewebfluxtutorial/publishersubscriber/PublisherSubscriberTest.java
+++ src/test/java/com/firat/reactivewebfluxtutorial/publishersubscriber/PublisherSubscriberTest.java
@@ -1,15 +1,22 @@
 package com.firat.reactivewebfluxtutorial.publishersubscriber;
 
 import org.junit.Test;
+import reactor.core.publisher.Mono;
 
 public class PublisherSubscriberTest {
 
     @Test
     public void publisherSubscriber(){
         Transaction transaction = new Transaction(1,1.25,"AMAZON Payment");
+
+        // Similar as 'Mono.just(transaction)', publisher receives the data
         TransactionPublisher publisher = new TransactionPublisher(transaction);
+
+        //`TransactionSubscriber` contains methods will be executed.
+        // The Mono object in WebFlux takes method arguments into its doOnNext, doOnError, doOnSuccess
         TransactionSubscriber subscriber = new TransactionSubscriber();
 
+        // behaves like Mono.just(transaction).subscribe()
         publisher.subscribe(subscriber);
         /**
          * 17:45:32.066 [main] INFO com.firat.reactivewebfluxtutorial.publishersubscriber.TransactionSubscriber - Publisher published the item: Transaction{id=1, amount=1.25, label='AMAZON Payment', status='CREATED'}, main
